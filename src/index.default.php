fdd6d5c10021837ee2f05d86dfe58310e0c5f068
Maxime Maillet <vagrant@SERVER>
2017-02-16 15:23:07 +0200

    Adapt example to singleton

--- src/index.default.php
+++ src/index.default.php
@@ -8,7 +8,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 try {
-  new ImpRouter('route.json');
+  ImpRouter::init('route.json');
 }
 catch(Exception $e) {
   echo 'Error :: '.$e->getMessage();
